e4ae673b8323aa98d11f0d8171e19a2937856af1
AVR21 <alejandroviera12@gmail.com>
2025-03-28 12:17:43 +0000

    Changed template load process from XHR to fetch API.

---
+++ src/js/common.js
@@ -0,0 +1,30 @@
+document.addEventListener("DOMContentLoaded", loadTemplates);
+
+function loadTemplates() {
+    document
+        .querySelectorAll("[get-template]")
+        .forEach(async element => {
+            try {
+                const res = await fetch(`${element.getAttribute("get-template")}`)
+                if (!res.ok) {
+                    console.log('Error al buscar el componente indicado')
+                    return
+                }
+                
+                element.innerHTML = await res.text()
+                
+
+            } catch (err) {
+                console.log(`Error en la carga del componente ${element.getAttribute("get-template")}:`, err)
+            }
+        })
+}
+
+async function fetchData() {
+    try {
+        const res = await fetch('data/db.json')
+        return await res.json()
+    } catch (err) {
+        console.error('Error al obtener datos:', err)
+    }
+}
