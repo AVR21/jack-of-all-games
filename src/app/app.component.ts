11a19606b1d7d76f300995db4b35d5dc7799d236
Luis Muñoz <lumusa2design@gmail.com>
2025-04-08 17:55:28 +0100

    Added Bootstrap to project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  templateUrl: './app.component.html', // Referencia al propio template del AppComponent
+  styleUrls: ['./app.component.css'],  // Archivo de estilos del AppComponent (opcional)
+  standalone: true,
+  imports: [HeaderComponent]  // Importa el HeaderComponent para poder usarlo en el template
 })
 export class AppComponent {
   title = 'jack-of-all-games';
