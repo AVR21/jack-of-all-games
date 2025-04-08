3e8f9d7f26566d87b348598f298ca5495b6137ae
alvaro <alvaroruzab@gmail.com>
2025-04-08 18:12:37 +0100

    mergeado luis w/ alvaro

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,7 +9,7 @@ import { AchievementCardComponent } from './achievement-card/achievement-card.co
   templateUrl: './app.component.html', // Referencia al propio template del AppComponent
   styleUrls: ['./app.component.css'],  // Archivo de estilos del AppComponent (opcional)
   standalone: true,
-  imports: [HeaderComponent, FooterComponent, AchievementCardComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, AchievementCardComponent],
 })
 export class AppComponent {
   title = 'jack-of-all-games';
