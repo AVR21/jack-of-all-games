47cc88f6dc2654592acea5e7addc72cfdc56ebf8
Luis Muñoz <lumusa2design@gmail.com>
2025-04-08 18:23:45 +0100

    Refactor modal on angular

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { ModalComponent } from './modal/modal.component';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
-  standalone: true
+  standalone: true,
+  imports: [ModalComponent]
 })
-export class HeaderComponent {}
+export class HeaderComponent { }
