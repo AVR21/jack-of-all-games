1e85c19bc44edc477cfd9e2a8065a764ee58c0ff
Luis Muñoz <lumusa2design@gmail.com>
2025-04-08 18:45:41 +0100

    Added carousel component and maincomponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import {CarouselComponent} from './main/carousel/carousel.component';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html', // Referencia al propio template del AppComponent
-  styleUrls: ['./app.component.css'],  // Archivo de estilos del AppComponent (opcional)
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
   standalone: true,
-  imports: [HeaderComponent]  // Importa el HeaderComponent para poder usarlo en el template
+  imports: [HeaderComponent, CarouselComponent]
 })
 export class AppComponent {
   title = 'jack-of-all-games';
