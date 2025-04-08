377eb01a748488fe71ac894fce8a07cdb5b66e28
alvaro <alvaroruzab@gmail.com>
2025-04-08 18:06:52 +0100

    Trabajado en footer & achievement-card

--- src/app/achievement-card/achievement-card.component.ts
+++ src/app/achievement-card/achievement-card.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-achievement-card',
+  standalone: true,
   templateUrl: './achievement-card.component.html',
   styleUrls: ['./achievement-card.component.css']
 })
-export class AchievementCardComponent {}
+export class AchievementCardComponent {
+  
+}
