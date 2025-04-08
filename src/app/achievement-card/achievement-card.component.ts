102ea7b792d8adde97f36c4dd89931d2bf3c118e
alvaro <alvaroruzab@gmail.com>
2025-04-08 16:27:35 +0100

    Footer & achievement-card pasados a Angular

---
+++ src/app/achievement-card/achievement-card.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-achievement-card',
+  templateUrl: './achievement-card.component.html',
+  styleUrls: ['./achievement-card.component.css']
+})
+export class AchievementCardComponent {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
+import { AchievementCardComponent } from './achievement-card/achievement-card.component';
 
 export const routes: Routes = [
     {path: 'footer', component: FooterComponent}
+    {path: 'achievement-card', component: AchievementCardComponent}
 ];
