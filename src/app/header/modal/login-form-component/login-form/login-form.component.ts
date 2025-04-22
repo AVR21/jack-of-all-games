190b5e8e494be4c5ba759de7903f4b3d7c319da0
Luis Muñoz <lumusa2design@gmail.com>
2025-04-22 23:30:18 +0100

    Added eye to password

--- src/app/header/modal/login-form-component/login-form/login-form.component.ts
+++ src/app/header/modal/login-form-component/login-form/login-form.component.ts
@@ -9,10 +9,13 @@ import {Component, signal} from '@angular/core';
 export class LoginFormComponent {
 showPassword = signal(false);
 type_text = signal('password');
+
+eye = signal('bi bi-eye-slash');
 visualizationPasword()
 {
   this.showPassword.set(!this.showPassword());
   this.showPassword() ? this.type_text.set('text') : this.type_text.set('password');
+  this.showPassword() ? this.eye.set('bi bi-eye') : this.eye.set('bi bi-eye-slash');
 }
 
 }
