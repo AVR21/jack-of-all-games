8a92936e0b7442176d424d208998d55fe362d208
Luis Muñoz <lumusa2design@gmail.com>
2025-04-25 18:14:33 +0100

    Deleted: Unused RouterLinks

--- src/app/header/modal/modal.component.ts
+++ src/app/header/modal/modal.component.ts
@@ -1,7 +1,6 @@
 import { Component, signal } from '@angular/core';
 import {LoginFormComponent} from './login-form-component/login-form/login-form.component';
 import {RegisterFormComponent} from './login-form-component/register-form/register-form.component';
-import {RouterLink, RouterLinkActive} from '@angular/router';
 @Component({
   selector: 'app-modal',
   imports: [LoginFormComponent, RegisterFormComponent],
@@ -10,8 +9,6 @@ import {RouterLink, RouterLinkActive} from '@angular/router';
 })
 export class ModalComponent {
 registerSelection = signal(true);
-classPaginationLogin = signal("page-link");
-classPaginationRegister = signal("page-link btn");
 changeRegisterSelection(sign : number) {
   if(sign == 0)
   {
