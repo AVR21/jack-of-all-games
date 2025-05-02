7e497a7d480216c99640cdff2b2ede1cd2518aad
AVR21 <alejandroviera12@gmail.com>
2025-05-02 11:34:49 +0100

    feat: Establish Firebase connection

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,13 +1,13 @@
-import {Component, signal} from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { ModalComponent } from './modal/modal.component';
-import {RouterLink, RouterLinkActive} from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css'],
   standalone: true,
-  imports: [ModalComponent, RouterLink, RouterLinkActive]
+  imports: [ModalComponent, RouterLink]
 })
 export class HeaderComponent
 {
