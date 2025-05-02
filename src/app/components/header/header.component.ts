921b900b4edce26bfd0081ea426a6fd28d8e8ab6
Luis Muñoz <lumusa2design@gmail.com>
2025-05-02 22:14:53 +0100

    Added methods to logout

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import {Component, OnInit, signal} from '@angular/core';
 import { ModalComponent } from './modal/modal.component';
 import { RouterLink } from '@angular/router';
+import {AuthService} from '../../services/auth/auth.service';
 
 @Component({
   selector: 'app-header',
@@ -9,8 +10,36 @@ import { RouterLink } from '@angular/router';
   standalone: true,
   imports: [ModalComponent, RouterLink]
 })
-export class HeaderComponent
+export class HeaderComponent implements OnInit
 {
   isRegistered = signal(false);
+
+  constructor(private auth: AuthService) {}
+  ngOnInit()
+  {
+    this.auth.getAuthState().subscribe(state =>
+    {
+      if (state)
+      {
+        this.isRegistered.set(true);
+        console.log("Fue mu bien");
+      } else
+      {
+        console.log("algo fue mal");
+      }
+    })
+  }
+
+  logOut()
+  {
+    this.auth.logout().subscribe(
+      {
+        next: () => { this.isRegistered.set(false); },
+        error: () => { console.log("logout"); },
+      });
+  }
+
+
+
 
 }
