adca5d2b31c9d2b42586457232b1d7b0818ba724
AVR21 <alejandroviera12@gmail.com>
2025-05-02 17:58:38 +0100

    feat: added Game interface & cover-card component.

---
+++ src/app/components/cover-card/cover-card.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cover-card',
+  imports: [],
+  templateUrl: './cover-card.component.html',
+  styleUrl: './cover-card.component.css'
+})
+export class CoverCardComponent {
+
+}
